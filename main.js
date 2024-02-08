e9fee51b3fe165e90f5c39edabf5986d948ad1e5
WandeTheWeeb <josefranciscosantesprimo@gmail.com>
2024-02-07 20:29:42 -0600

    Confetti

--- main.js
+++ main.js
@@ -32,3 +32,32 @@ function generateTulipan() {
 // Llamamos a la función al cargar la página y al cambiar el tamaño de la ventana
 window.onload = generateTulipan;
 window.onresize = generateTulipan;
+
+
+const defaults = {
+    spread: 360,
+    ticks: 100,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
+    shapes: ["heart"],
+    colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+  };
+  
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 2,
+  });
+  
+  confetti({
+    ...defaults,
+    particleCount: 25,
+    scalar: 3,
+  });
+  
+  confetti({
+    ...defaults,
+    particleCount: 10,
+    scalar: 4,
+  });
